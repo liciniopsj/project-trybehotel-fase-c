e4b09a6d1e273b6679a73825c410647e7ada5492
liciniopsj <licinio.psj@gmail.com>
2023-08-30 17:56:13 -0300

    Refactor: GetRooms

--- src/TrybeHotel/Repository/RoomRepository.cs
+++ src/TrybeHotel/Repository/RoomRepository.cs
@@ -29,7 +29,8 @@ namespace TrybeHotel.Repository
                         Name = room.Hotel.Name,
                         Address = room.Hotel.Address,
                         CityId = room.Hotel.CityId,
-                        CityName = room.Hotel.City.Name
+                        CityName = room.Hotel.City.Name,
+                        State = room.Hotel.City.State
                     }
                 }).ToList();
         }
